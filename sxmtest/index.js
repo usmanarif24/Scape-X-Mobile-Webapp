580c03827762c3dcd3a0776a065a4d0fea60dcee
Erich Querner <querner@interactive-scape.com>
2024-05-27 16:00:56 +0200

    use anchor tag to prevent load in new tab.

--- sxmtest/index.js
+++ sxmtest/index.js
@@ -1,9 +1,5 @@
 function init() {
-    let startButton = document.getElementById('start-button');
-    startButton.addEventListener("click", () => {
-        let roomInput = document.getElementById('room_id_input');
-        open(`sxmtest.html?r=${roomInput.value}`);
-    });
+    let roomInput = document.getElementById('room_id_input');
+    window.location.href = `sxmtest.html?r=${roomInput.value}`;
 }
-init();
-export {};
+
