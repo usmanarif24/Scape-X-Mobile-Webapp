3a68cd5c236c47bfa40710022a45e4983a0a0d7f
Erich Querner <querner@interactive-scape.com>
2024-06-24 09:24:09 +0200

    refactor icon update to own method

--- sxmtest/js/app.js
+++ sxmtest/js/app.js
@@ -18,36 +18,39 @@ class App {
         roomIdElement.innerHTML = this.session.roomId;
         this.session.start();
         setInterval(() => {
-            let moving = this.session.device.isMoving;
-            let tilted = this.session.device.isTilted;
-            let sensorIcon = document.getElementById("sensorIcon");
-            sensorIcon.classList.add("show");
-            if (this.session.device.joined) {
-                if (moving) {
-                    if (sensorIcon.innerHTML != "vibration")
-                        sensorIcon.innerHTML = "vibration";
-                }
-                else {
-                    if (sensorIcon.innerHTML != "smartphone")
-                        sensorIcon.innerHTML = "smartphone";
-                }
-                let sensorIcon2 = document.getElementById("sensorIcon2");
-                sensorIcon2.classList.add("show");
-                if (tilted) {
-                    if (sensorIcon2.innerHTML != "north_east")
-                        sensorIcon2.innerHTML = "north_east";
-                }
-                else {
-                    if (sensorIcon2.innerHTML != "horizontal_rule")
-                        sensorIcon2.innerHTML = "horizontal_rule";
-                }
-            }
-            else {
-                if (sensorIcon.innerHTML != "no_cell")
-                    sensorIcon.innerHTML = "no_cell";
-            }
+            this.updateMotionIcons();
         }, 250);
     }
 
+    updateMotionIcons() {
+        let moving = this.session.device.isMoving;
+        let tilted = this.session.device.isTilted;
+        let sensorIcon = document.getElementById("sensorIcon");
+        sensorIcon.classList.add("show");
+        if (this.session.device.joined) {
+            if (moving) {
+                if (sensorIcon.innerHTML != "vibration")
+                    sensorIcon.innerHTML = "vibration";
+            }
+            else {
+                if (sensorIcon.innerHTML != "smartphone")
+                    sensorIcon.innerHTML = "smartphone";
+            }
+            let sensorIcon2 = document.getElementById("sensorIcon2");
+            sensorIcon2.classList.add("show");
+            if (tilted) {
+                if (sensorIcon2.innerHTML != "north_east")
+                    sensorIcon2.innerHTML = "north_east";
+            }
+            else {
+                if (sensorIcon2.innerHTML != "horizontal_rule")
+                    sensorIcon2.innerHTML = "horizontal_rule";
+            }
+        }
+        else {
+            if (sensorIcon.innerHTML != "no_cell")
+                sensorIcon.innerHTML = "no_cell";
+        }
+    }
 }
 export { App };
