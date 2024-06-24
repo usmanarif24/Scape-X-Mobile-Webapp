d90a5b38a6ee2df5559de12c738fd6a218b387c2
Erich Querner <querner@interactive-scape.com>
2024-06-24 09:30:10 +0200

    create session and set name in init method

--- sxmtest/js/app.js
+++ sxmtest/js/app.js
@@ -1,10 +1,6 @@
 class App {
     constructor() {
-
         this.displayName = "";
-        this.session = new SxmWeb.SxmSession();
-        let roomIdElement = document.getElementById("span-room-id");
-        roomIdElement.innerHTML = this.session.roomId;
         this.appRoot = document.getElementById('app');
         this.startButton = document.getElementById('start-button');
         this.startButton.addEventListener("click", () => {
@@ -16,7 +12,9 @@ class App {
         if (this.sxmInitialized)
             return;
         this.sxmInitialized = true;
-
+        this.session = new SxmWeb.SxmSession();
+        let roomIdElement = document.getElementById("span-room-id");
+        roomIdElement.innerHTML = this.session.roomId;
         this.session.start();
         setInterval(() => {
             this.updateMotionIcons();
